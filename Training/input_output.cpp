47abbd304276dbbd88c5827a7ac010bd69f462d2
Brett Groshong <norioz@gmail.com>
2018-08-02 11:27:05 -0700

    Adds solution to 'Input Output' exercise.

---
+++ Training/input_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "input_output.h"
+
+int hackerrank::input_output() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << a + b + c << std::endl;
+    return 0;
+}
---
+++ Training/input_output.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// hackerrank: Input and Output
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+// Task
+// Read 3 numbers from stdin and print their sum to stdout.
+// Input Format
+// A single line containing 3 space-separated integers: a, b, and c.
+
+namespace hackerrank {
+
+int input_output();
+
+}
