ddd14868124cb79d8d8706e75a128c13e07f83c7
Brett Groshong <norioz@gmail.com>
2018-08-06 10:13:05 -0700

    Adds hackerrank Strings exercise.

--- Training/CPP_Basic.cpp
+++ Training/CPP_Basic.cpp
@@ -1,5 +1,5 @@
 #include <cstdlib>
-#include "stringstream.h"
+#include "strings.h"
 
 // The entry point for the CPP_Basic project.
 // The main function here works as a driver that executes
@@ -10,7 +10,7 @@ int main()
 {
     int result = 1;
 
-    result = hackerrank::stringstream();
+    result = hackerrank::strings();
     
     system("pause");
     return result;
---
+++ Training/strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "strings.h"
+
+using namespace std;
+
+int hackerrank::strings()
+{
+    string a, b;
+    cin >> a >> b;
+
+    // print the string sizes
+    cout << a.size() << " " << b.size() << endl;
+
+    // print the concattenation
+    cout << a + b << endl;
+
+    // swap the first characters
+    char t = a[0];
+    a[0] = b[0];
+    b[0] = t;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
