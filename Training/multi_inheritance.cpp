18efc504d3b985563bad1907bdefffe61edbbe37
Brett Groshong <norioz@gmail.com>
2018-08-17 17:21:51 -0700

    Add hackerrank 'Mulit Level Interhitance' exercise.

---
+++ Training/multi_inheritance.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "multi_inheritance.h"
+
+using namespace std;
+
+// START HACKERRANK GIVEN CODE
+class Triangle {
+public:
+    void triangle() {
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle {
+public:
+    void isosceles() {
+        cout << "I am an isosceles triangle\n";
+    }
+};
+// END HACKERRANK GIVEN CODE
+
+class Equilateral : public Isosceles {
+public:
+    void equilateral()
+    {
+        cout << "I am an equilateral triangle\n";
+    }
+};
+
+// START HACKERRANK GIVEN CODE
+int hackerrank::multi_inheritance() {
+
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
+// END HACKERRANK GIVEN CODE
---
+++ Training/multi_inheritance.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// hackerrank: Multi Level Inheritance
+// https://www.hackerrank.com/challenges/multi-level-inheritance-cpp/problem
+// 
+// Suppose we have a class A which is the base class and we have a class B which is 
+// derived from class A and we have a class C which is derived from class B, we can
+// access the functions of both class A and class B by creating an object for class
+// C. This is called Multi-Level inheritance. (B inherits A and C inherits B.)
+// Create a class called Equilateral which inherits from Isosceles and should have a
+// function such that the output is as given below.
+//
+// I am an equilateral triangle
+// I am an isosceles triangle
+// I am a triangle
+
+namespace hackerrank {
+    
+int multi_inheritance();
+
+}
