b0f148ceba38b5dd1ad85836d16269c1435ea13d
Brett Groshong <norioz@gmail.com>
2018-08-20 23:35:08 -0700

    Update 'Attribute Parser' exercise to use string and vector.

--- Training/attribute_parser.cpp
+++ Training/attribute_parser.cpp
@@ -1,131 +1,147 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define MAX_LINES 20
-#define MAX_LINE_LENGTH 200
-#include <cstdio>
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 #include "attribute_parser.h"
 
-bool is_element_close(char* element, char* line)
-{
-    if (*(line + 1) != '/') {
-        return false;
-    }
-    int closeNameLength = strcspn(line + 2, ">");
-    int elementNameLength = strcspn(element + 1, "> ");
-    if (closeNameLength != elementNameLength) {
-        return false;
-    }
-    for (int i = 0; i < elementNameLength; ++i) {
-        if ((element + 1)[i] != (line + 2)[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+using namespace std;
 
-bool element_matches(char* element, char* query)
-{
-    int elementNameLength = strcspn(element + 1, "> ");
-    int queryWordLength = strcspn(query, ".~");
-    if (elementNameLength != queryWordLength) {
-        return false;
-    }
-    for (int i = 0; i < elementNameLength; ++i) {
-        if ((element + 1)[i] != query[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+struct Node {
+    string tag = "UNSET";
+    vector<string> attributes;
+    vector<string> values;
+};
 
-char* find_query_match(char* hrml, char* query)
+Node line_to_node(string& s)
 {
-    // If the current line of hrml doesn't start with
-    // < then it isn't HRML.
-    if (*hrml != '<') {
-        return NULL;
-    }
-    // If the current line represents an element close
-    // this steps us up the HRML tree and the query is
-    // no longer matching.
-    if (*(hrml + 1) == '/') {
-        return NULL;
-    }
-    
-    // If the current element start matches the query
-    // either the query is exhaused or update and recurse.
-    if (element_matches(hrml, query)) {
-        char* pch = strchr(query, '.');
-        if (pch == NULL) {  // found the last element of the query
-            return hrml;
-        }
-        else {
-            return find_query_match(hrml + MAX_LINE_LENGTH, pch + 1);
-        }
-    }
-    else {  // Move beyond the current element and end or recurse
-        int lineToConsider = 1;
-        while (!is_element_close(hrml, hrml + (MAX_LINE_LENGTH * lineToConsider))) {
-            lineToConsider++;
-        }
-        return find_query_match(hrml + (MAX_LINE_LENGTH * (lineToConsider + 1)), query);
-    }
-}
+    Node result;
 
-char* get_attribute(char* element, char* key)
-{
-    char fullKey[MAX_LINE_LENGTH];
-    strcpy(fullKey, key);
-    fullKey[strlen(key) - 1] = '\0';
-    strcat(fullKey, " = ");
-    char * pntr = strstr(element, fullKey);
-    if (pntr == NULL) {
-        return NULL;
+    // If the string starts with "</" then
+    // create and end element Node.
+    if (s[2] == '/') {
+        result.tag = "END";
+        return result;
+    }
+
+    // Otherwise s represents the start of an element.
+    stringstream ss(s);
+    bool endFound = false;
+
+    // process the tag
+    string tmp_str = "UNSET";
+    ss >> tmp_str;
+    if (tmp_str[tmp_str.size() - 1] == '>') { // the element has no attributes
+        endFound = true;                      // <tag>
+        result.tag = tmp_str.substr(1, tmp_str.size() - 1);
     }
     else {
-        return pntr + strlen(fullKey) + 1;
+        result.tag = tmp_str.substr(1, tmp_str.size());
     }
+
+    // process the attributes
+    char tmp_char = '\0';
+    while (!endFound) {
+        ss >> tmp_str;
+        if (tmp_str == ">") {  // Found a floating close angle brace.
+            endFound = true;
+            break;
+        }
+        result.attributes.push_back(tmp_str);
+        ss >> tmp_char;  // drop the = in a attr = "val" statement
+        ss >> tmp_str;
+        if (tmp_str[tmp_str.size() - 1] == '>') {  // ends with the end open-element brace
+            tmp_str = tmp_str.substr(1, tmp_str.size() - 3);  // prune the "">
+            result.values.push_back(tmp_str);
+            endFound = true;
+        }
+        else {
+            tmp_str = tmp_str.substr(1, tmp_str.size() - 2);    // prune the "" off the value 
+            result.values.push_back(tmp_str);
+        }
+    }
+    return result;
 }
 
 int hackerrank::attribute_parser()
 {
-    int n, q;
-    scanf("%d %d\n", &n, &q);
+    int n = -1, // the number of HRML lines
+        q = -1; // the number of query lines
+    string line = "UNSET";
+    getline(cin, line);
+    stringstream ss(line);
+    ss >> n >> q;
 
-    // read the HRML lines
-    char hrml[MAX_LINES * MAX_LINE_LENGTH];
-    for (int i = 0; i < n; i++) {
-        fgets(hrml + i * MAX_LINE_LENGTH, MAX_LINE_LENGTH, stdin);
-    }
+    vector<Node> nodes;
+    vector<int> endIdxs;
 
-    // parse the queries
-    // search for the results
-    // and store them
-    char results[MAX_LINES * MAX_LINE_LENGTH];
-    char query[MAX_LINE_LENGTH];
-    for (int i = 0; i < q; i++) {
-        fgets(query, MAX_LINE_LENGTH, stdin);
-        char* match = find_query_match(hrml, query);
-        if (match == NULL) {
-            strcpy(results + MAX_LINE_LENGTH * i, "Not Found!");
+    vector<int> startIdxStack;
+    for (int i = 0; i < n; ++i) {
+        getline(cin, line);
+        Node n = line_to_node(line);
+        nodes.push_back(n);
+        endIdxs.push_back(-1);
+        if (n.tag == "END") {
+            endIdxs[startIdxStack[startIdxStack.size() - 1]] = i;
+            startIdxStack.pop_back();
         }
         else {
-            char* value = get_attribute(match, strchr(query, '~') + 1);
-            if (value == NULL) {
-                strcpy(results + MAX_LINE_LENGTH * i, "Not Found!");
+            startIdxStack.push_back(i);
+        }
+    }
+
+    for (int i = 0; i < q; ++i) {
+        string query = "UNSET";
+        getline(cin, query);
+        
+        string tagChain = "UNSET";
+        string attribute = "UNSET";
+        
+        // Split the tag chain and target attribute apart.
+        size_t pos = query.find("~");
+        tagChain = query.substr(0, pos);
+        attribute = query.substr(pos + 1, query.size());
+
+        // Turn the tag chain into a vector of tags.
+        vector<string> tags;
+        stringstream ss(tagChain);
+        string tag = "UNSET";
+        while (getline(ss, tag, '.')) {
+            tags.push_back(tag);
+        }
+        
+        unsigned int nodeIdx = 0;
+        int tagIdx = 0;
+        while (nodeIdx < nodes.size()) {
+            Node currentNode = nodes[nodeIdx];
+            if (currentNode.tag == "END") {   // A query can't step up levels so
+                cout << "Not Found!" << endl; // finding an end node fails the query.
+                break;
             }
-            else {
-                int num = strcspn(value, "\"");
-                strncpy(results + MAX_LINE_LENGTH * i, value, num);
-                (results + MAX_LINE_LENGTH * i)[num] = '\0';
+            else if (currentNode.tag == tags[tagIdx]) {
+                if ((tagIdx + 1) == tags.size()) {
+                    unsigned int k = 0;
+                    while (k < currentNode.attributes.size()) {
+                        if (currentNode.attributes[k] == attribute) {
+                            cout << currentNode.values[k] << endl;
+                            break;
+                        }
+                        ++k;
+                    }
+                    if (k == currentNode.attributes.size()) {
+                        cout << "Not Found!" << endl;
+                    }
+                    break;
+                }
+                else {
+                    ++tagIdx;   // move to the next tag
+                    ++nodeIdx;  // step into this node
+                }
+            }
+            else {  // Move beyond the current element's end and try to match.
+                nodeIdx = endIdxs[nodeIdx] + 1;
             }
         }
     }
-    
-    // print the results
-    for (int i = 0; i < q; ++i) {
-        printf("%s\n", results + i * MAX_LINE_LENGTH);
-    }
+
     return 0;
 }
