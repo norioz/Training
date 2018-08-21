f9bab48ea300033e0e0dfdacbfae62ae8a32c93c
esmelusina <esmelusina@gmail.com>
2018-08-21 10:34:28 -0700

    Update rectangle_area.cpp

--- Training/rectangle_area.cpp
+++ Training/rectangle_area.cpp
@@ -5,52 +5,50 @@ using namespace std;
 
 class Rectangle {
 protected:
-    int m_width = -1,
-        m_height = -1;
+	int m_width  = -1;
+	int m_height = -1;
 public:
-    virtual void display()
-    {
-        cout << m_width << " " << m_height << endl;
-    }
+	virtual void display () {
+		cout << m_width << " " << m_height << endl;
+	}
 };
 
 class RectangleArea : public Rectangle {
 public:
-    void read_input()
-    {
-        cin >> m_width >> m_height;
-    }
-    void display()
-    {
-        cout << (m_width * m_height) << endl;
-    }
+	void read_input () {
+		cin >> m_width >> m_height;
+	}
+
+	void display () override {
+		cout << (m_width * m_height) << endl;
+	}
 };
 
 int hackerrank::rectangle_area()
 {
-    // START HACKERRANK GIVEN CODE
+	// START HACKERRANK GIVEN CODE
 
-    /*
-     * Declare a RectangleArea object
-    */
-    RectangleArea r_area;
+	/*
+	* Declare a RectangleArea object
+	*/
+	RectangleArea r_area;
 
-    /*
-     * Read the width and height
-     */
-    r_area.read_input();
+	/*
+	* Read the width and height
+	*/
+	r_area.read_input();
 
-    /*
-     * Print the width and height
-     */
-    r_area.Rectangle::display();
+	/*
+	* Print the width and height
+	*/
+	r_area.Rectangle::display();
 
-    /*
-     * Print the area
-     */
-    r_area.display();
+	/*
+	* Print the area
+	*/
+	r_area.display();
 
-    return 0;
+	return 0;
 
-    // END HACKERRANK GIVEN CODE
+	// END HACKERRANK GIVEN CODE
 }
